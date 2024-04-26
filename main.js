f226ade3c10142514f554416eceb3b87b3af0400
Roseanedasilva <167696930+Roseanedasilva1212@users.noreply.github.com>
2024-04-26 19:32:18 -0300

    Repositório: Jogo do Mario

---
+++ main.js
@@ -0,0 +1,31 @@
+const mario= document.querySelector('.mario');
+
+const jump =(){
+    mario.classList.add('jump');
+
+     setTimeout(() {
+        mario.classList.remove('jump';)
+     }, 500);
+}
+
+const loop =setInterval(() {
+  const pipePosition = pipe.offsetLeft;
+
+  if(pipePosition 120){
+
+    pipe.style.animation ='none';
+    pipe.style.left='${pipePosition}px';
+  }
+
+
+},10) ;
+
+
+
+
+
+
+
+document.addEventListener('keydown',jump);
+
+
